5a5a0327b39dbba29be8e83bc5b2cd4f74c6d44f
ce-milovan.bekic <ce-milovan.bekic@bearingpoint.com>
2019-12-20 14:35:20 +0000

    Work 20191220

---
+++ chepter_3/names.py
@@ -0,0 +1,18 @@
+# define list of friends names
+names = ['kico', 'nenad', 'daco', 'slobodan']
+# print friends names one at the time
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+# print message with friends names
+print(f"This is my friend {names[0]}")
+print(f"This is my friend {names[1]}")
+print(f"This is my friend {names[2]}")
+print(f"This is my friend {names[3]}")
+
+cars = ['audi', 'vw', 'bmw']
+print(f"I would like to own an {cars[0]}")
+print(f"I would like to own an {cars[1]}")
+print(f"I would like to own an {cars[2]}")
