cb2b5eb0ba310b67010d0de44df3704efc147c74
ce-milovan.bekic <ce-milovan.bekic@bearingpoint.com>
2019-12-23 14:09:08 +0000

    Work 20191223 page 39

---
+++ chepter_3/motocycles.py
@@ -0,0 +1,22 @@
+motocycles = ['honda', 'yamaha', 'suzuki']
+print(motocycles)
+
+#motocycles[0] = 'ducati'
+#print(motocycles)
+
+# add item to beginning of the list using append
+motocycles.append('ducati')
+print(motocycles)
+
+# add item to list on position with insert
+motocycles = ['honda', 'yamaha', 'suzuki']
+print(motocycles)
+motocycles.insert(0, 'ducati')
+print(motocycles)
+
+# delete item from list using del
+motocycles = ['honda', 'yamaha', 'suzuki']
+print(motocycles)
+del motocycles[0]
+print(motocycles)
+
