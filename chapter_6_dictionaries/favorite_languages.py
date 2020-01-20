4cd280dc5b37bb683129e740e4bd64c6c2069189
Milovan Bekic <milovanbekic@gmail.com>
2020-01-21 00:00:47 +0100

    Page 105 try it yourself

--- chapter_6_dictionaries/favorite_languages.py
+++ chapter_6_dictionaries/favorite_languages.py
@@ -1,3 +1,5 @@
+# dictionary structure:  name_of_dict = {'key': 'value', 'key2': 'value2'}
+
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
@@ -6,4 +8,43 @@ favorite_languages = {
 }
 
 language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}")
+print(f"\nSarah's favorite language is {language}")
+
+
+# items() method return all key-value pairs
+for name, language in favorite_languages.items():
+    print(f"{name.title()} favorite language is {language.title()}")
+
+# keys() method return only keys from dictionary - default behavior of for loop 
+for name in favorite_languages.keys():
+    print(name.title())
+# do the same thing:
+for name in favorite_languages:
+    print(name.title())
+
+firends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in firends:
+        # define variable language with value of name key mention in friends list
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take out the poll!")
+
+# sort keys by alphabetic order
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+
+
+# for loop through dictionary with values() method to return values of keys
+print("\nThe following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+# set() print all values from dictionary without repeat
+print("\nThe following languages without repeat:")
+for language in set(favorite_languages.values()):
+    print(language.title())
---
+++ chapter_6_dictionaries/user.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+# loop through dictionary and return key-value pairs > item() method
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
