85ea58861292789c3f33bc4e201d0fd29aa5f8d8
ce-milovan.bekic <ce-milovan.bekic@bearingpoint.com>
2020-01-20 14:27:22 +0000

    Work 20200120 page 99

---
+++ chapter_6_dictionaries/person.py
@@ -0,0 +1,18 @@
+person = {'first_name': 'kico', 'last_name': 'marjanovic', 'age': 28, 'city': 'banjaluka'}
+print(f"{person['first_name'].title()}")
+print(f"{person['last_name'].title()}")
+print(f"{person['age']}")
+print(f"{person['city'].title()}")
+
+favorite_number = {
+    'kico': 17,
+    'sreco': 19,
+    'marko': 55,
+    'sasa': 27,
+    'kole': 33,
+}
+print(f"Favorite number is {favorite_number['kico']}")
+print(f"Favorite number is {favorite_number['sreco']}")
+print(f"Favorite number is {favorite_number['marko']}")
+print(f"Favorite number is {favorite_number['sasa']}")
+print(f"Favorite number is {favorite_number['kole']}")
