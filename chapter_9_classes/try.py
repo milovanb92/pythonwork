85c127f769848cb90914fdbd10e040d9cfbfbba6
Milovan Bekic <milovanbekic@gmail.com>
2020-03-13 23:40:44 +0100

    Work page 173

--- chapter_9_classes/try.py
+++ chapter_9_classes/try.py
@@ -2,24 +2,35 @@ class Resturant:
     def __init__(self, resturant_name, cuisine_type):
         self.name = resturant_name
         self.cuisine = cuisine_type
+        self.number_served = 1
     
     def describe_resturant(self):
         print(f"Name of this resturatn is {self.name} and cuisine is {self.cuisine}")
     
     def open_resturant(self):
         print(f"The resturan {self.name} is open.")
+    
+    def set_number_served(self, served):
+        self.number_served = served
+    
+    def increment_number_served(self, increment):
+        self.number_served += increment
 
 resturant = Resturant('Kanjoj', 'Domaca')
 
-print(f"My resturant is called {resturant.name}")
-print(f"Cuisine in my resturant is {resturant.cuisine}")
-resturant.describe_resturant()
-resturant.open_resturant()
+# print(f"My resturant is called {resturant.name}")
+# print(f"Cuisine in my resturant is {resturant.cuisine}")
+# resturant.describe_resturant()
+# resturant.open_resturant()
 
-resturant2 = Resturant('Konoba', 'Rostilj')
-print(f"Moj restoran je {resturant2.name} a najbolji im je {resturant2.cuisine}.")
-resturant2.describe_resturant()
+# resturant2 = Resturant('Konoba', 'Rostilj')
+# print(f"Moj restoran je {resturant2.name} a najbolji im je {resturant2.cuisine}.")
+# resturant2.describe_resturant()
 
-resturant3 = Resturant('Zlatna kruna', 'Ope Rostilj')
-print(f"Moj restoran je {resturant3.name} a imaju {resturant3.cuisine}.")
-resturant3.describe_resturant()
+# resturant3 = Resturant('Zlatna kruna', 'Ope Rostilj')
+# print(f"Moj restoran je {resturant3.name} a imaju {resturant3.cuisine}.")
+# resturant3.describe_resturant()
+
+resturant.set_number_served(4)
+resturant.increment_number_served(3)
+print(f"Number of served customers are {resturant.number_served}")
--- chapter_9_classes/try2.py
+++ chapter_9_classes/try2.py
@@ -4,19 +4,40 @@ class User:
         self.last = last_name
         self.age = user_age
         self.hair = hair_color
+        self.login_attempts = 0
     
     def describe_user(self):
         print("\nThis are information about user: ")
-        print(f"\tFirst name is: {self.first} \n\tLast name is: {self.last} \n\tUser age: {self.age} \n\tHair color: {self.hair}")
+        print(f"\tFirst name is: {self.first} \n\tLast name is: {self.last} \n\tUser age: {self.age} \n\tHair color: {self.hair} \n\tLogin Attempts: {self.login_attempts}")
 
     def greet_user(self):
         print(f"Thank you for joining us {self.first} {self.last}")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+    
+
 user1 = User('Milovan', 'Bekic', 27, 'Brown')
 user2 = User('Kico', 'Marjanovic', 28, 'Black')
+user3 = User('Marko', 'Vukovic', 27, 'Black')
+
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
 
 user1.describe_user()
-user1.greet_user()
 
-user2.describe_user()
-user2.greet_user()
+user1.reset_login_attempts()
+user1.describe_user()
+# user1.greet_user()
+
+# user2.describe_user()
+# user2.greet_user()
+
+# user3.describe_user()
+# user3.greet_user()
+
